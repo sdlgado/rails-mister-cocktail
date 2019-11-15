1ed4911a0cc0211a9e950997f40a7a1cfd8878ab
Sergio Delgado <sergio@dgtl.ec>
2019-11-15 15:41:15 +0100

    rails update

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
-  has_many_attached :photos
+  has_one_attached :photos
   validates :name, uniqueness: true, presence: true
 end
